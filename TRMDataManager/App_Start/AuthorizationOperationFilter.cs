e2adc96fe5a1d1e6b4353c99ec80c93b03b05722
sysadmin <sysadmin@MSI>
2022-09-04 22:09:33 +0300

    Implement Swagger

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.Swagger;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        #region Implementation of IOperationFilter
+
+        /// <summary>
+        /// Applies the operation filter.
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiDescription"></param>
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+
+            #endregion
+        }
+    }
+}
